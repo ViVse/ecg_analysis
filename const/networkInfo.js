ffe548e0eab0a60db1e3f5a45c3ef18cc6758eab
ViVse <ivanisenko730@gmail.com>
2023-12-12 14:59:59 +0200

    add network prediction info

---
+++ const/networkInfo.js
@@ -0,0 +1,10 @@
+const accuracyInfo = {
+  anomaly: 99,
+  normal: 97,
+  ["supraventicular ectopic beat"]: 86,
+  ["venticular ectopic beat"]: 74,
+  ["fusion beat"]: 70,
+  ["unknown beat"]: 95,
+};
+
+export default accuracyInfo;
